52b2e10be37217d3ea4c1c04a1334e524c2345a6
Bruno <bruzzo.bruno@gmail.com>
2023-06-15 13:41:27 -0300

    cuerpo artc

--- lanacionparser.py
+++ lanacionparser.py
@@ -1,12 +1,12 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, NavigableString, Tag
 import requests
-from datetime import datetime
+from datetime import datetime, timedelta, timezone
 import sqlite3
 import pandas as pd 
 import pickle
 from trie import Trie
 
-pickle_file = 'ids.pickle'
+pickle_file = '/root/media-parser/ids.pickle'
 
 id_dict = Trie()
 
@@ -14,10 +14,14 @@ with open(pickle_file, "rb") as file:
     id_dict = pickle.load(file)
 
 #Creacion de DB y creacion del cursor.
-con = sqlite3.connect("/Users/bruno/Documents/lanacion-parser/medios.db")
+con = sqlite3.connect("/root/media-parser/medios.db")
 cur = con.cursor()
 
-date = datetime.now()
+
+# Crear una timezone GMT-3 y obtener el now para GMT-3.
+gmt_minus_3 = timezone(timedelta(hours=-3))
+date = datetime.now(tz=gmt_minus_3)
+
 medio = 'lanacion'
 print("\n\n====="+"PARSEO DEL " + str(date) +"=====\n\n")
 url = "https://lanacion.com.ar"
@@ -26,14 +30,12 @@ response = requests.get(url)
 
 soup = BeautifulSoup(response.content,'html.parser')
 
-agregados = 0
 estados = soup.find_all("article")
 for x in estados:
     id = 0
     articulo = x.contents[0].contents[0]
     titulo = articulo.get("title")
     href = articulo.get("href")
-    print(titulo)
 
     #ingrear al articulo para obtener el potencial ID
     article_url = "https://lanacion.com.ar"+ href
@@ -43,22 +45,38 @@ for x in estados:
     potential_id_tag = meta_tags[14]
     if potential_id_tag.get('name') == "vf:container_id":
         id = meta_tags[14].get('content')
-        print("POTENCIAL ID: " + str(id))
-        print("el ID esta presente: " + str(id_dict.search(id)))
         if(id_dict.search(id) == False):
-            #query de insertado de articulo
-            query = "INSERT INTO articulos (id, timestamp, medio, titulo, url) VALUES (?, ?, ?, ?, ?)"
-            values = (id, date, medio, titulo, href)
-            cur.execute(query, values)
-            id_dict.insert(id)
-            print("el ID esta presente: " + str(id_dict.search(id)))
-            print("---AGREGADO---\n")
-            agregados = agregados + 1
+            #extraer categoria
+            categoria = href.split("/")[1]
+            #extraer cuerpo articulo
+            cuerponota_object = article_soup.find_all(attrs={"class":"cuerpo__nota"})
+            if(len(cuerponota_object) > 0):
+                cuerponota = cuerponota_object[0]
+                parrafos = cuerponota.find_all('p')
+                str_articulo = ""
+                for x in parrafos:
+                    print("cant de parrafos " + str(len(x)))
+                    for i in range(len(x)):
+                        print("parrafo " + str(i))
+                        print(x.contents[i])
+                        if(isinstance(x.contents[i], NavigableString)):
+                            str_articulo = str_articulo + x.contents[i]
+                        if(isinstance(x.contents[i], Tag) & len(x.contents[i]) > 0):
+                            print("es un tag y dentro tiene " + str(len(x.contents[i])) + " cosas")
+                            str_articulo = str_articulo + str(x.contents[i].contents[0])
+                    str_articulo = str_articulo + '\n\n'
+                contenido = str_articulo.replace("<strong>", "").replace("</strong>","")
+                #query de insertado de articulo
+                query = "INSERT INTO articulos (id, timestamp, medio, titulo, url, categoria, contenido) VALUES (?, ?, ?, ?, ?, ?, ?)"
+                values = (id, date, medio, titulo, href, categoria, contenido)
+                print(values)
+                cur.execute(query, values)
+                id_dict.insert(id)
 # Guardar los cambios en la base de datos
 con.commit()
-
+con.close()
 #guardar Trie
 with open(pickle_file, "wb") as file:
     pickle.dump(id_dict, file)
 
-print(str(agregados) + " articulos agregados.")
+print("--------_FIN_---------")
